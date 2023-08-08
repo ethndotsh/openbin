637269166fb480cc0e3f8e500735c440b3937910
curse <58047023+ethndotsh@users.noreply.github.com>
2023-08-07 23:50:06 -0600

    organization, svg bg

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,11 +1,12 @@
 import { Logo } from "@/components/logo";
 import { Footer } from "@/components/footer";
-import { SupabaseLogo } from "@/components/supabase-logo";
+import { SupabaseLogo } from "@/components/svg/supabase-logo";
+import { Circles } from "@/components/svg/circles";
 import { Button } from "@/components/ui/button";
 import { Balancer } from "react-wrap-balancer";
 import { Terminal } from "@/components/terminal";
 import Link from "next/link";
-import bg from "@/assets/bg.jpg";
+import bg from "@/assets/bg.svg";
 import Image from "next/image";
 
 export default function Home() {
@@ -15,59 +16,13 @@ export default function Home() {
         <div className="absolute left-0 top-0 z-0 flex h-full w-full items-center justify-center">
           <Image
             src={bg}
-            priority
             className="h-full w-full object-cover object-center"
+            priority
             alt="background"
           />
         </div>
         <div className="absolute left-0 top-0 z-0 flex h-full w-full items-center justify-center">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-full"
-            viewBox="0 0 1059 982"
-            fill="none"
-          >
-            <mask
-              id="mask0_99_154"
-              style={{ maskType: "alpha" }}
-              maskUnits="userSpaceOnUse"
-              x="0"
-              y="-67"
-              width="1059"
-              height="1059"
-            >
-              <rect
-                y="-67"
-                width="1059"
-                height="1059"
-                fill="url(#paint0_radial_99_154)"
-              />
-            </mask>
-            <g mask="url(#mask0_99_154)">
-              <circle cx="529.5" cy="462.5" r="66" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="124" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="182" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="239" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="297" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="356" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="414" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="471" stroke="#CACACA" />
-              <circle cx="529.5" cy="462.5" r="529" stroke="#CACACA" />
-            </g>
-            <defs>
-              <radialGradient
-                id="paint0_radial_99_154"
-                cx="0"
-                cy="0"
-                r="1"
-                gradientUnits="userSpaceOnUse"
-                gradientTransform="translate(529.5 462.5) rotate(90) scale(529.5)"
-              >
-                <stop stopColor="white" />
-                <stop offset="1" stopColor="white" stopOpacity="0" />
-              </radialGradient>
-            </defs>
-          </svg>
+          <Circles />
         </div>
 
         <section className="z-10 flex items-center pb-12 pt-36 md:py-56">
