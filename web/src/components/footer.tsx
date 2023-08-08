91d7bd1dea51da17299b376b988e3495b225651e
curse <58047023+ethndotsh@users.noreply.github.com>
2023-08-07 23:26:09 -0600

    center footer

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
   return (
-    <div className="absolute bottom-2 left-0 right-0 ml-3 mr-3 text-center text-white md:bottom-4 md:ml-auto md:mr-auto">
-      <p className="text-sm font-medium text-neutral-600">
+    <div className="absolute bottom-2 left-0 right-0 ml-3 mr-3 text-center text-white md:bottom-4 md:mx-auto">
+      <p className="text-center text-sm font-medium text-neutral-600">
         Made with 💚 by Ethan, Jack,{" "}
         <a href="https://github.com/LavenderFoxxo">Alexander</a> and Mathis
       </p>
