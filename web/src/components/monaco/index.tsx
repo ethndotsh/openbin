eb0a3e4382634f577b40b12e1d7830a4ade24f87
curse <58047023+ethndotsh@users.noreply.github.com>
2023-08-08 21:00:11 -0600

    gaa!!

--- web/src/components/monaco/index.tsx
+++ web/src/components/monaco/index.tsx
@@ -5,17 +5,19 @@ import { useEffect, useState } from "react";
 import dynamic from "next/dynamic";
 import theme from "@/assets/gh-light.json";
 import type monacoTypes from "monaco-editor";
+import { useSearchParams } from "next/navigation";
 
 import { Session } from "@supabase/supabase-js";
 import { Navbar } from "./navbar";
-
-
 
 const Monaco = dynamic(() => import("@monaco-editor/react"), { ssr: false });
 
 export function MonacoEditor({ session }: { session: Session | null }) {
   const monaco = useMonaco();
   const [selectedLanguage, setLanguage] = useState("plaintext");
+  const [publishOpen, setPublishOpen] = useState(false);
+  const [value, setValue] = useState<string | undefined>("Welcome to Openbin!");
+  const searchParams = useSearchParams();
 
   useEffect(() => {
     if (monaco) {
@@ -24,19 +26,33 @@ export function MonacoEditor({ session }: { session: Session | null }) {
         theme as monacoTypes.editor.IStandaloneThemeData,
       );
       monaco.editor.setTheme("gh-light");
+
+      if (localStorage && searchParams.get("publish")) {
+        const previousData = localStorage.getItem("editor-data");
+        if (previousData) {
+          setValue(previousData);
+          setPublishOpen(true);
+          // prompt publish
+        }
+      }
     }
-  }, [monaco]);
+  }, [monaco, searchParams]);
   return (
     <div>
       <Navbar
         session={session}
         selectedLanguage={selectedLanguage}
         setLanguage={setLanguage}
+        value={value}
+        publishOpen={publishOpen}
+        setPublishOpen={setPublishOpen}
       />
       <div className="w-full border-b" />
       <Monaco
         height="90vh"
         theme="gh-light"
+        value={value}
+        onChange={setValue}
         options={{
           padding: {
             top: 16,
@@ -51,7 +67,7 @@ export function MonacoEditor({ session }: { session: Session | null }) {
         }}
         language={selectedLanguage}
         defaultLanguage="plaintext"
-        defaultValue="Welcome to Openbin!!"
+        defaultValue="Welcome to Openbin!"
       />
     </div>
   );
---
+++ web/src/components/monaco/publish.tsx
@@ -0,0 +1,5 @@
+"use server";
+
+export async function publish(data: unknown) {
+    console.log(data)
+}
