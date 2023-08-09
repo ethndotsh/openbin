dbbf47b62717274a84e962d16c1bb882c45b3fed
Mathis <account@mathis.sh>
2023-08-09 12:42:32 -0400

    (web) tags to link components in footer

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -1,25 +1,27 @@
+import Link from "next/link";
+
 const Footer = () => {
   return (
     <div className="absolute ml-3 mr-3 bottom-0 text-center md:bottom-4 z-50 text-white md:ml-auto md:mr-auto left-0 right-0">
       <p className="text-sm text-neutral-600 font-medium">
         Made with 💚 by{" "}
-        <a href="https://github.com/ethndotsh" target="blank">
+        <Link href="https://github.com/ethndotsh" target="blank">
           <span className="font-bold">Ethan</span>
-        </a>
+        </Link>
         ,{" "}
-        <a href="https://github.com/jackmerrill" target="blank">
+        <Link href="https://github.com/jackmerrill" target="blank">
           <span className="font-bold">
             Jack
           </span>
-        </a>
+        </Link>
         ,{" "}
-        <a href="https://github.com/LavenderFoxxo" target="blank">
+        <Link href="https://github.com/LavenderFoxxo" target="blank">
           <span className="font-bold">Alexander</span>
-        </a>{" "}
+        </Link>{" "}
         and{" "}
-        <a href="https://github.com/mathislajs" target="blank">
+        <Link href="https://github.com/mathislajs" target="blank">
           <span className="font-bold">Mathis</span>
-        </a>
+        </Link>
       </p>
     </div>
   );
