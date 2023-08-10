b63ac78d57045a97c1d1a4a8b6638e2b31e13819
curse <58047023+ethndotsh@users.noreply.github.com>
2023-08-09 18:59:17 -0600

    install redirects

--- web/next.config.js
+++ web/next.config.js
@@ -3,6 +3,22 @@ const nextConfig = {
   experimental: {
     serverActions: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/install.sh",
+        destination:
+          "https://raw.githubusercontent.com/ethndotsh/openbin/main/install.sh",
+        permanent: false,
+      },
+      {
+        source: "/install.ps1",
+        destination:
+          "https://raw.githubusercontent.com/ethndotsh/openbin/main/install.ps1",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
