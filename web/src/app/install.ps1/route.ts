a7844cbedcff50628f3732eedff0ccaedcd8adc1
curse <58047023+ethndotsh@users.noreply.github.com>
2023-08-09 22:01:11 -0600

    switch to edge routes

--- web/src/app/install.ps1/route.ts
+++ web/src/app/install.ps1/route.ts
@@ -1,6 +1,6 @@
-import { NextRequest, NextResponse } from "next/server";
+export const runtime = "edge";
 
-export async function GET(req: NextRequest) {
+export async function GET() {
   // Fetches the install script from GitHub
   const res = await fetch(
     "https://raw.githubusercontent.com/ethndotsh/openbin/main/install.ps1",
