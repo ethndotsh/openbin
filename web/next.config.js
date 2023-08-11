b58b0f23ef9f24a750ac2115d159eafdf85853ad
Jack Merrill <me@jackmerrill.com>
2023-08-10 19:15:15 -0500

    feat(web): draft middleware

--- web/next.config.js
+++ web/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
     serverActions: true,
   },
   images: {
-    domains: ["avatars.githubusercontent.com"],
+    domains: ["avatars.githubusercontent.com", "api.dicebear.com"],
   },
   async redirects() {
     return [];
