d08c799367470a7020c47ce89efbda41e76a6745
curse <58047023+ethndotsh@users.noreply.github.com>
2023-08-12 13:54:02 -0700

    whoops forgot the theme!!

--- web/src/components/editor/viewer.tsx
+++ web/src/components/editor/viewer.tsx
@@ -26,6 +26,20 @@ export function PasteViewer({
 }) {
   const monaco = useMonaco();
 
+  useEffect(() => {
+    if (monaco) {
+      monaco.editor.defineTheme(
+        "gh-light",
+        theme as monacoTypes.editor.IStandaloneThemeData,
+      );
+      monaco.editor.setTheme("gh-light");
+      monaco.languages.typescript.typescriptDefaults.setDiagnosticsOptions({
+        noSemanticValidation: true,
+        noSyntaxValidation: true,
+      });
+    }
+  }, [monaco]);
+
   return (
     <div>
       <Monaco
