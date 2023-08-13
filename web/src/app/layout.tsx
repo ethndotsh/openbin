c46f0e10c2dc48a881622993e40cb32a73fce8f4
Matthew RONCHETTO <heyy@maatt.fr>
2023-08-13 10:43:43 -0700

    fix: remove unnecessary import

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import Script from "next/script";
 import { Toaster } from "sonner";
 
 const inter = Inter({ subsets: ["latin"] });
