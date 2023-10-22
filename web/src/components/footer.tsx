671fe2d73d330526e92f882786cf07531a582ff1
Mathis <account@mathis.sh>
2023-10-22 14:56:37 -0400

    sum

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -3,36 +3,10 @@ import Link from "next/link";
 const Footer = () => {
   return (
     <div className="absolute bottom-0 left-0 right-0 z-50 ml-3 mr-3 text-center text-white md:bottom-4 md:mx-auto">
-      {/* no point in having this if the pages dont exist */}
-      {/* <p className="text-sm font-medium text-neutral-600">
-        <Link href="/privacy-policy">
-          <span className="font-bold">Privacy Policy</span>
-        </Link>{" "}
-        /{" "}
-        <Link href="/fair-use">
-          <span className="font-bold">Fair Use</span>
-        </Link>{" "}
-        /{" "}
-        <Link href="https://github.com/ethndotsh/openbin" target="_blank">
-          <span className="font-bold">Open Source</span>
-        </Link>
-      </p> */}
       <p className="text-sm font-medium text-neutral-600">
         Made with 💚 by{" "}
-        <Link href="https://github.com/ethndotsh" target="_blank">
-          <span className="font-semibold">Ethan</span>
-        </Link>
-        ,{" "}
-        <Link href="https://github.com/jackmerrill" target="_blank">
-          <span className="font-semibold">Jack</span>
-        </Link>
-        ,{" "}
-        <Link href="https://github.com/LavenderFoxxo" target="_blank">
-          <span className="font-semibold">Alexander</span>
-        </Link>{" "}
-        and{" "}
-        <Link href="https://github.com/mathislajs" target="_blank">
-          <span className="font-semibold">Mathis</span>
+        <Link href="https://unnamed.engineering" target="_blank">
+          <span className="font-semibold">Unnamed Engineering</span>
         </Link>
       </p>
     </div>
